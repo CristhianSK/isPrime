5734066972481a79f17ac91411796a17cf35148f
CristhianSK <cristhianpreis@gmail.com>
2025-08-27 16:27:00 -0300

    verificador de numeros primos

---
+++ Project1.dpr
@@ -0,0 +1,57 @@
+program Project1;
+
+{$APPTYPE CONSOLE}
+
+{$R *.res}
+
+uses
+  System.SysUtils;
+
+function IsPrime(const aNumero: integer): Boolean;
+var i : Integer;
+begin
+  if aNumero <= 1 then begin
+   result := False;
+   exit;
+  end;
+
+  for i := 2 to aNumero -1 do begin
+    if (aNumero mod i = 0) then begin
+     result:= False;
+     Exit;
+    end;
+  end;
+
+  result := True;
+
+
+
+end;
+
+
+var
+  numero: Integer;
+begin
+  try
+
+    Writeln('Digite um numero para verificar se é primo:');
+    try
+      Readln(numero);
+    except
+      WriteLn('Digite apenas números');
+      Exit;
+    end;
+
+    if isPrime(numero) then begin
+      WriteLn('O número ' +numero.ToString +' é primo!');
+    end else begin
+      WriteLn('O número ' +numero.ToString +' NÃO é primo!');
+    end;
+  Readln(numero);
+
+
+  except
+    on E: Exception do
+      Writeln(E.ClassName, ': ', E.Message);
+  end;
+end.
